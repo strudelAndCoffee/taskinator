bb27cff35a7c28cebe779b09de7f14379462c2c3
Bort <93967399+strudelAndCoffee@users.noreply.github.com>
2022-01-15 00:05:14 -0600

    load error persists, must be a browser issue

--- assets/js/script.js
+++ assets/js/script.js
@@ -189,14 +189,14 @@ var saveTasks = function() {
 
 var loadTasks = function() {
     var savedTasks = localStorage.getItem("tasks");
-  
-    if (!savedTasks) {
-      return false;
-    }
-  
-    savedTasks = JSON.parse(savedTasks);
-    for (var i = 0; i < savedTasks.length; i++) {
 
+    if (!savedTasks) {
+        return false;
+    }
+
+    savedTasks = JSON.parse(savedTasks);
+
+    for (var i = 0; i < savedTasks.length; i++) {
         createTaskEl(savedTasks[i]);
     }
 };
